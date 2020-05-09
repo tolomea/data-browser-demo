2c0f6268933c167e11979ea6eac852c2e81cc792
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-05-09 12:41:01 +0100

    hack auth to always be logged in as the only user

---
+++ conf/middleware.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+
+
+class AuthenticationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.user = User.objects.get()
+        return self.get_response(request)
--- conf/settings.py
+++ conf/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "conf.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
