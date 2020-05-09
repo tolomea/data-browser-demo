94f88c9041f9e6a7b537747e2940923a7107fa99
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-05-09 15:21:06 +0100

    admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User
+from django.contrib.auth.admin import UserAdmin as AbstractUserAdmin
+from . import models
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ["user", "name", "price", "onsale"]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = models.OrderItem
+    fields = ["product", "price", "quantity", "total"]
+    readonly_fields = ["total"]
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    fields = ["submitted_time", "buyer", "total"]
+    readonly_fields = ["total"]
+    inlines = [OrderItemInline]
+
+
+@admin.register(models.User)
+class UserAdmin(AbstractUserAdmin):
+    fieldsets = list(AbstractUserAdmin.fieldsets) + [
+        ("History", {"fields": ["last_purchase"]})
+    ]
