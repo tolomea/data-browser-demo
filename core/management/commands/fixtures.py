f7f7da3b48f10201241533210a217d3f3cf0659c
Gordon Wrigley <gordon.wrigley@gmail.com>
2020-05-17 10:35:46 +0100

    add some saved views to the fixtures

--- core/management/commands/fixtures.py
+++ core/management/commands/fixtures.py
@@ -8,6 +8,8 @@ import faker_microservice
 import random
 from django.utils import timezone
 
+
+NUM_USERS = 50
 PRODUCT_ONSALE = 0.9
 USER_HAS_PRODUCT = 0.5
 USER_HAS_ORDER = 0.75
@@ -25,16 +27,13 @@ def rand(probability):
 class Command(BaseCommand):
     help = "Create demo site data"
 
-    def handle(self, *args, **options):
-        fake = Faker()
-        fake.add_provider(faker_microservice.Provider)
-
+    def create_admin_user(self):
         self.stdout.write(self.style.SUCCESS("creating admin user"))
         admin = models.User.objects.create(
             email="admin@example.org",
             username="admin",
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
+            first_name=self.fake.first_name(),
+            last_name=self.fake.last_name(),
             is_staff=True,
             is_active=True,
         )
@@ -44,17 +43,40 @@ class Command(BaseCommand):
         view_content_type = ContentType.objects.get_for_model(View)
         all_ddb_view_perms = Permission.objects.filter(content_type=view_content_type)
         admin.user_permissions.set(list(all_can_view_perms) + list(all_ddb_view_perms))
+        return admin
 
+    def create_views(self, user):
+        View.objects.create(
+            owner=user,
+            name="All Products",
+            description="All Products currently on sale with prices and sellers, ordered by price.",
+            model_name="core.Product",
+            fields="id,name,price-0,user__first_name,user__last_name,user__email",
+            query="onsale__equals=true",
+        )
+        View.objects.create(
+            owner=user,
+            name="People who brought databases",
+            description=(
+                "Everyone who brought some kind of database and what kind it was. "
+                "We're using random data so it's possible there aren't any."
+            ),
+            model_name="core.OrderItem",
+            fields="product__name,product__user__first_name,product__user__last_name",
+            query="product__name__contains=database",
+        )
+
+    def create_users_and_products(self):
         self.stdout.write(self.style.SUCCESS("creating users and products"))
         users = []
         products = []
-        for i in range(100):
-            first_name = fake.first_name()
+        for i in range(NUM_USERS):
+            first_name = self.fake.first_name()
             user = models.User.objects.create(
-                email=fake.email(),
+                email=self.fake.email(),
                 username=f"{first_name}{i:02}",
                 first_name=first_name,
-                last_name=fake.last_name(),
+                last_name=self.fake.last_name(),
                 is_staff=False,
                 is_active=True,
             )
@@ -64,12 +86,14 @@ class Command(BaseCommand):
                 products.append(
                     models.Product.objects.create(
                         user=user,
-                        name=fake.microservice(),
+                        name=self.fake.microservice(),
                         price=price(),
                         onsale=rand(PRODUCT_ONSALE),
                     )
                 )
+        return users, products
 
+    def create_orders(self, users, products):
         self.stdout.write(self.style.SUCCESS("creating orders"))
 
         def make_order_item():
@@ -83,7 +107,7 @@ class Command(BaseCommand):
 
         for user in users:
             while rand(USER_HAS_ORDER):
-                submitted_time = timezone.make_aware(fake.date_time_this_decade())
+                submitted_time = timezone.make_aware(self.fake.date_time_this_decade())
                 order = models.Order.objects.create(
                     buyer=user, submitted_time=submitted_time
                 )
@@ -92,3 +116,12 @@ class Command(BaseCommand):
                 make_order_item()
                 while rand(ORDER_HAS_EXTRA_ITEMS):
                     make_order_item()
+
+    def handle(self, *args, **options):
+        self.fake = Faker()
+        self.fake.add_provider(faker_microservice.Provider)
+
+        admin = self.create_admin_user()
+        self.create_views(admin)
+        users, products = self.create_users_and_products()
+        self.create_orders(users, products)
